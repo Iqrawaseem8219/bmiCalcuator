6cd1d05f6102a4adf59fca34be5c9221a64ed0ac
Iqrawaseem8219 <iqrawaseem674@gmail.com>
2025-04-28 01:35:49 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let weight = parseFloat(prompt("Enter your weight in kilograms (kg):"));
+
+let height = parseFloat(prompt("Enter your height in meters (m):"));
+
+let bMI = weight / (height * height);
+
+alert("Your BMI is: " + bMI);
